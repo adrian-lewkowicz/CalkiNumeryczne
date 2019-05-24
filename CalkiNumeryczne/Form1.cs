fc2aef9cfba84d97ee122c8bd8849d1eb635703e
Adrian Lewkowicz <adrian.lewkowicz.biala@gmail.com>
2019-05-24 03:22:40 +0200

    metoda Trapezów

--- CalkiNumeryczne/Form1.cs
+++ CalkiNumeryczne/Form1.cs
@@ -16,5 +16,46 @@ namespace CalkiNumeryczne
         {
             InitializeComponent();
         }
+        double threePower;
+        double twoPower;
+        double onePower;
+        double A;
+        double B;
+        int lp;
+        double[] x = new double[100];
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            initializeValues();
+            double[,] tab = new double[lp + 1, lp + 1];
+            for(int i = 0; i < tab.GetLength(0); i++)
+            {
+                tab[0, i] = A + ((double)i / (double)lp);
+                listBox1.Items.Add(tab[0, i]);
+                tab[1, i] = oblicz(tab[0, i]);
+                listBox1.Items.Add(tab[1, i]);
+            }
+            double h = ((B-A) / lp)/2;
+            double suma = 0;
+            for (int i = 1; i < tab.GetLength(0)-1; i++)
+            {
+                suma += 2*tab[1, i];
+            }
+            double wynik = suma * h;
+            EqualsTextBox.Text = wynik.ToString();
+        }
+        void initializeValues()
+        {
+            A = (double)AnumericUpDown.Value;
+            B = (double)BnumericUpDown.Value;
+            threePower = (double)ThreePowernumericUpDown.Value;
+            twoPower = (double)TwonumericUpDown.Value;
+            onePower = (double)PowernumericUpDown.Value;
+            lp = (int)EPSnumericUpDown.Value;
+        }
+        double oblicz(double x)
+        {
+            return threePower* Math.Pow(x, 3) +twoPower* Math.Pow(x, 2) + onePower*Math.Pow(x, 1)*Math.Sin(x) ;
+        }
+
     }
 }
